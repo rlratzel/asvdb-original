b5a3e04be1e7c8dd484e867baa55d35517a9dd19
Rick Ratzel <rratzel@nvidia.com>
2019-09-19 21:26:00 -0700

    Fixed issues resulting from not using cartesian product of param values, fixed version number to be int instead of float.

--- asvdb/asvdb.py
+++ asvdb/asvdb.py
@@ -1,8 +1,8 @@
-import copy
 import json
 import os
 from os import path
 import fcntl
+import itertools
 
 
 class BenchmarkResult:
@@ -12,8 +12,11 @@ class BenchmarkResult:
     """
     def __init__(self, funcName, argNameValuePairs, result):
         self.name = funcName
-        self.argNameValuePairs = copy.deepcopy(argNameValuePairs)
+        self.argNameValuePairs = self.__sanitizeArgNameValues(argNameValuePairs)
         self.result = result
+
+    def __sanitizeArgNameValues(self, argNameValuePairs):
+        return [(n, str(v if v is not None else "NaN")) for (n, v) in argNameValuePairs]
 
 
 class BenchmarkInfo:
@@ -43,7 +46,7 @@ class ASVDb:
     confFileName = "asv.conf.json"
     defaultResultsDirName = "results"
     defaultHtmlDirName = "html"
-    defaultConfVersion = 1.0
+    defaultConfVersion = 1
     benchmarksFileName = "benchmarks.json"
     machineFileName = "machine.json"
 
@@ -55,7 +58,7 @@ class ASVDb:
         """
         self.dbDir = dbDir
         self.confFilePath = path.join(self.dbDir, self.confFileName)
-        d = self.__getJsonDictFromFile(self.confFilePath)
+        d = self.__loadJsonDictFromFile(self.confFilePath)
 
         self.confVersion = self.defaultConfVersion
         self.resultsDirName = d.setdefault("results_dir", self.defaultResultsDirName)
@@ -65,7 +68,7 @@ class ASVDb:
 
         d["repo"] = repo
         d["branches"] = branches or []
-        d["version"] = 1.0
+        d["version"] = 1
 
         # FIXME: consider a separate method for writing this file, ctor may not
         # be appropriate
@@ -113,7 +116,7 @@ class ASVDb:
             newParamNames.append(n)
             newParamValues.append(v)
 
-        d = self.__getJsonDictFromFile(self.benchmarksFilePath)
+        d = self.__loadJsonDictFromFile(self.benchmarksFilePath)
 
         benchDict = d.setdefault(benchmarkResult.name,
                                  self.__getDefaultBenchmarkDescrDict(
@@ -122,42 +125,28 @@ class ASVDb:
         existingParamNames = benchDict["param_names"]
         existingParamValues = benchDict["params"]
 
+        numExistingParams = len(existingParamNames)
+        numExistingParamValues = len(existingParamValues)
+        numNewParams = len(newParamNames)
+
         # Check for the case where a result came in for the function, but it has
         # a different number of args vs. what was saved previously
-        numExistingParams = len(existingParamNames)
-        numNewParams = len(newParamNames)
         if numExistingParams != numNewParams:
-            raise ValueError("result for %s had %d args in benchmarks.json, "
-                             "but new result has %d args" \
+            raise ValueError("result for %s had %d params in benchmarks.json, "
+                             "but new result has %d params" \
                              % (benchmarkResult.name, numExistingParams,
                                 numNewParams))
         numParams = numNewParams
 
-        # assume the number of param values for each param is the same
-        if existingParamValues:
-            numExistingParamValues = len(existingParamValues[0])
-        else:
-            numExistingParamValues = 0
-
-        # check if the set of args is aleady present (eg. just a re-run of the
-        # same benchmark) and if so do not add the param values again.
-        skip = False
-        for i in range(numExistingParamValues):
-            evals = []
-            for vals in existingParamValues:
-                evals.append(vals[i])
-            if newParamValues == evals:
-                print("Benchmark with param values already exist, not adding to benchmarks.json")
-                skip = True
-                break
-
-        if not skip:
+        cartProd = list(itertools.product(*existingParamValues))
+        if tuple(newParamValues) not in cartProd:
             if numExistingParamValues == 0:
                 for newVal in newParamValues:
                     existingParamValues.append([newVal])
             else:
                 for i in range(numParams):
-                    existingParamValues[i].append(newParamValues[i])
+                    if newParamValues[i] not in existingParamValues[i]:
+                        existingParamValues[i].append(newParamValues[i])
 
         d[benchmarkResult.name] = benchDict
 
@@ -182,7 +171,7 @@ class ASVDb:
         resultsFilePath = path.join(self.resultsDirPath,
                                     benchmarkInfo.machineName,
                                     self.machineFileName)
-        d = self.__getJsonDictFromFile(resultsFilePath)
+        d = self.__loadJsonDictFromFile(resultsFilePath)
         d["arch"] = benchmarkInfo.arch
         d["cpu"] = benchmarkInfo.cpuType
         d["gpu"] = benchmarkInfo.gpuType
@@ -235,7 +224,7 @@ class ASVDb:
         resultsFilePath = path.join(self.resultsDirPath,
                                     benchmarkInfo.machineName,
                                     "%s.json" % (benchmarkInfo.commitHash))
-        d = self.__getJsonDictFromFile(resultsFilePath)
+        d = self.__loadJsonDictFromFile(resultsFilePath)
         d["params"] = {"gpu": benchmarkInfo.gpuType,
                        "cuda": benchmarkInfo.cudaVer,
                        "machine": benchmarkInfo.machineName,
@@ -243,32 +232,56 @@ class ASVDb:
                        "python": benchmarkInfo.pythonVer,
                        }
         d["requirements"] = {}
-        resultsDict = d.setdefault("results", {})
-        resultDict = resultsDict.setdefault(benchmarkResult.name, {})
+        allResultsDict = d.setdefault("results", {})
+        resultDict = allResultsDict.setdefault(benchmarkResult.name, {})
 
-        resultParamValues = [v for (_, v) in benchmarkResult.argNameValuePairs]
-
-        # Transform the params list to: [[v1A, v1B, v1C], [v2A, v2B, v2C]]
-        # for easy searching.
-        existingParamValuesList = [list(l) for l in zip(*resultDict.setdefault("params", []))]
-
+        existingParamValuesList = resultDict.setdefault("params", [])
         existingResultValueList = resultDict.setdefault("result", [])
 
-        # check if the set of param values are already in the 'params' list, and
-        # if so, just update the corresponding result
-        if resultParamValues in existingParamValuesList:
-            i = existingParamValuesList.index(resultParamValues)
-            existingResultValueList[i] = benchmarkResult.result
+        # FIXME: dont assume these are ordered properly (ie. the same way as
+        # defined in benchmarks.json)
+        newResultParamValues = tuple(v for (_, v) in benchmarkResult.argNameValuePairs)
 
-        # if these are new param values, then append to the existing set of
-        # param values and append another result
+        # Update the "params" lists with the new param settings for the new result.
+        # Only add values that are not already present
+        numExistingParamValues = len(existingParamValuesList)
+        if numExistingParamValues == 0:
+            for newParamValue in newResultParamValues:
+                existingParamValuesList.append([newParamValue])
+            results = [benchmarkResult.result]
+
         else:
-            existingParamValuesList.append(resultParamValues)
-            existingResultValueList.append(benchmarkResult.result)
-            # transform the params list back to:
-            # [[v1A, v2A], [v1B, v2B], [v1C, v2C]]
-            # and store it now that it has new params
-            resultDict["params"] = [list(l) for l in zip(*existingParamValuesList)]
+            for i in range(numExistingParamValues):
+                if newResultParamValues[i] not in existingParamValuesList[i]:
+                    existingParamValuesList[i].append(newResultParamValues[i])
+
+            # ASV uses the cartesian product of the param values for looking up
+            # the result for a particular combination of param values.  For
+            # example: "params": [ ["a"], ["b", "c"], ["d", "e"] results in:
+            # [("a", "b", "d"), ("a", "b", "e"), ("a", "c", "d"), ("a", "c",
+            # "e")] and each combination of param values has a result, with the
+            # results for the corresponding param values in the same order.  If
+            # a result for a set of param values DNE, use None.
+
+            # store existing results in map based on cartesian product of all
+            # current params.
+            paramsCartProd = list(itertools.product(*existingParamValuesList))
+            # Assume there is an equal number of results for cartProd values
+            # (some will be None)
+            paramsResultMap = dict(zip(paramsCartProd, existingResultValueList))
+
+            # Add the new result
+            paramsResultMap[newResultParamValues] = benchmarkResult.result
+
+            # Re-compute the cartesian product of all param values now that the
+            # new values are added. Use this to determine where to place the new
+            # result in the result list.
+            results = []
+            for paramVals in itertools.product(*existingParamValuesList):
+                results.append(paramsResultMap.get(paramVals))
+
+        resultDict["params"] = existingParamValuesList
+        resultDict["result"] = results
 
         d["commit_hash"] = benchmarkInfo.commitHash
         d["date"] = int(benchmarkInfo.commitTime)
@@ -290,7 +303,7 @@ class ASVDb:
                 }
 
 
-    def __getJsonDictFromFile(self, jsonFile):
+    def __loadJsonDictFromFile(self, jsonFile):
         """
         Return a dictionary representing the contents of jsonFile by
         either reading in the existing file or returning {}
