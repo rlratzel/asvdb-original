8ece63c045985a29558b6dec7a2b576f94d01532
Rick Ratzel <rratzel@nvidia.com>
2020-02-25 17:15:50 -0600

    Bumped version to 0.1.5dev0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="ASVDb",
-      version="0.1.4dev0",
+      version="0.1.5dev0",
       packages=["asvdb",
                 ],
       license="Apache 2.0",
